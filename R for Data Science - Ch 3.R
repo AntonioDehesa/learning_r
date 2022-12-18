819811f3555d922c7e8bffca1741a859cdcd34b2
Antonio Dehesa <hdehesaortiz@gmail.com>
2022-12-17 22:48:15 -0600

    Completed chapter 3

--- R for Data Science - Ch 3.R
+++ R for Data Science - Ch 3.R
@@ -29,3 +29,63 @@ select(flights, -(year:day)) # select every col except from year to day
 rename(flights, tail_num = tailnum)
 select(flights, time_hour, air_time, everything())
 select(flights, air_time,air_time,air_time)
+
+flights_sml <- select(flights, year: day,
+                      ends_with("delay"),
+                      distance, 
+                      air_time)
+flights_sml
+mutate(flights_sml,
+       gain = arr_delay - dep_delay,
+       speed = distance / air_time * 60)
+
+transmute(flights, 
+          gain = arr_delay - dep_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain/hours)
+# You can create pipelines, in order to avoid naming every 
+# intermediate result
+delays <- flights %>%
+  group_by(dest) %>%
+  summarize(
+    count = n(),
+    dist = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)
+  ) %>%
+  filter(count>20, dest != "HNL")
+delays
+# The symbol %>% is the pipeline, wich can be read as "then". 
+# Select 
+select(flights, year, month, day)
+?summarize
+not_cancelled <- flights %>%
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+delays <- not_cancelled %>%
+  group_by(tailnum) %>%
+  summarize(
+    delay = mean(arr_delay)
+  )
+ggplot( data = delays, mapping = aes(x = delay)) + 
+  geom_freqpoly(binwidth = 10)
+delays <- not_cancelled %>%
+  group_by(tailnum) %>%
+  summarize(
+    delay = mean(arr_delay, na.rm = TRUE),
+    n = n()
+  )
+ggplot( data = delays, mapping = aes(x = n, y = delay)) + 
+  geom_point(alpha = 1/10)
+delays %>%
+  filter(n > 25) %>%
+  ggplot(mapping = aes(x = n, y = delay)) +
+  geom_point(alpha = 1/10)
+# Exercises
+x <- not_cancelled %>% count(dest)
+y <- not_cancelled %>% count(tailnum, wt = distance)
+res1 <- not_cancelled %>%
+  group_by(dest) %>%
+  summarise(n = n())
+# These two seem to be the same, so its ok
+res2 <- not_cancelled %>% 
+  group_by(tailnum) %>%
+  tally(distance)
